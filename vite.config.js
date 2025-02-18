67099c3682cfdec541aabed5a64987f57b8d86ae
Ujjwal <ujjwalgoel104@gmail.com>
2025-02-18 13:26:30 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base:'/Loginpage/',
+  build: {
+    outDir: 'dist',
+  },
+
   plugins: [react(),
   tailwindcss(),],
 
